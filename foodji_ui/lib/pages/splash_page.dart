547805e3d057d47e51d2dfa2eb9d47383a667544
Francis Sarrazin <sarrazinfrancis@live.ca>
2022-09-25 22:19:14 -0400

    WIP - Splash toujours non fonctionnel
    Une rencontre sera prévue pour tenter de débloquer cette partie du travail et mieux comprendre pourquoi certains widgets n'agissent pas comme ils le devraient, de même que pourquoi le debugger (en direct) ne fonctionne pas.

--- foodji_ui/lib/pages/splash_page.dart
+++ foodji_ui/lib/pages/splash_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../cubit/app_cubit_states.dart';
+import '../cubit/app_cubits.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -11,30 +15,60 @@ class SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Container(
-      width: double.maxFinite,
-      height: double.maxFinite,
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('img/background-gradient.png'),
-              fit: BoxFit.cover)),
-      child: Container(
-          margin: const EdgeInsets.only(left: 20),
-          height: double.maxFinite,
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('img/background-bookmark.png'),
-                  fit: BoxFit.fill)),
-          child:
-              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-            Container(
-                margin: const EdgeInsets.only(left: 20),
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('img/splash-fr.png'),
-                        fit: BoxFit.fill)))
-          ])),
-    )));
+        body: Container(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('img/background-gradient.png'),
+                    fit: BoxFit.fill))));
   }
 }
+
+// class SplashPageState extends State<SplashPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return BlocBuilder<AppCubits, CubitStates>(builder: (context, state) {
+//       return Scaffold(
+//           body: PageView.builder(
+//               scrollBehavior: null,
+//               itemBuilder: (_, index) {
+//                 return Container(
+//                   width: double.maxFinite,
+//                   height: double.maxFinite,
+//                   child: Stack(children: [
+//                     Positioned(
+//                         top: 0,
+//                         left: 0,
+//                         child: Container(
+//                             width: double.maxFinite,
+//                             height: double.maxFinite,
+//                             decoration: const BoxDecoration(
+//                                 image: DecorationImage(
+//                                     image: AssetImage(
+//                                         'img/background-gradient.png'),
+//                                     fit: BoxFit.fill)))),
+//                     Positioned(
+//                         top: 0,
+//                         left: 20,
+//                         child: Container(
+//                             height: double.maxFinite,
+//                             decoration: const BoxDecoration(
+//                                 image: DecorationImage(
+//                                     image: AssetImage(
+//                                         'img/background-bookmark.png'),
+//                                     fit: BoxFit.contain)))),
+//                     Positioned(
+//                         top: 0,
+//                         left: 20,
+//                         child: Container(
+//                             decoration: const BoxDecoration(
+//                                 image: DecorationImage(
+//                                     image: AssetImage('img/splash-fr.png'),
+//                                     fit: BoxFit.contain))))
+//                   ]),
+//                 );
+//               }));
+//     });
+//   }
+// }
