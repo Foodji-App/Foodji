68437b04972edf21894c589d16fca956e8a0ae0a
Éloyze Brière-Alix <brie1301@usherbrooke.ca>
2022-10-07 20:05:10 -0400

    UnitType French and constructor

--- foodji_api/Domain/Recipes/UnitType.cs
+++ foodji_api/Domain/Recipes/UnitType.cs
@@ -6,22 +6,51 @@ public record UnitType
     
     public string Name { get; private set; }
     
-    public static UnitType Gram = new UnitType("Gram");
-    public static UnitType Millilitre = new UnitType("Millilitre");
-    public static UnitType Unit = new UnitType("Unit");
-    public static UnitType Cup = new UnitType("Cup");
-    public static UnitType TeaSpoon = new UnitType("TeaSpoon");
-    public static UnitType TableSpoon = new UnitType("TableSpoon");
-    public static UnitType FluidOunce = new UnitType("FluidOunce");
-    public static UnitType Ounce = new UnitType("Ounce");
-    public static UnitType Pound = new UnitType("Pound");
+    public static UnitType Unit = new UnitType("unité");
+    public static UnitType Millilitre = new UnitType("ml");
+    public static UnitType Gram = new UnitType("g");
+    public static UnitType Cup = new UnitType("tasse");
+    public static UnitType TableSpoon = new UnitType("c. à table");
+    public static UnitType TeaSpoon = new UnitType("c. à thé");
+    public static UnitType FluidOunce = new UnitType("fl oz");
+    public static UnitType Ounce = new UnitType("oz");
+    public static UnitType Pound = new UnitType("lb");
 
-    public UnitType(string name)
+    private UnitType(string name)
     {
         Name = name;
     }
+
+    public static UnitType Create(string name)
+    {
+        // TODO reconcile FE and BE on translation / uniformity here
+        switch (name.ToLower())
+        {
+            // As in "one banana" - avoid adding additional arbitrary measures as much as possible, they are not uniform.
+            // "A can" can be entirely different from one area to another, from a year to another (e.g. shrinkflation), etc.
+            // TODO - have a tooltip to discourage such use on the front end whenever possible
+            case "unite":
+            case "unité":
+                return Unit;
+            case "ml":
+            case "millilitre":
+                return Millilitre;
+            case "g":
+            case "gramme":
+                return Gram;
+            case "t.":
+            case "tasse":
+                return Cup;
+            case "c. à soupe":
+            case "c. a soupe":
+                return TableSpoon;
+            case "c. à thé":
+            case "c. a the":
+                return TeaSpoon;
+            default:
+                throw new DomainException($"Unrecognized unit type {name}");
+        }
+    }
     
-    // As in "one banana" - avoid adding additional arbitrary measures as much as possible, they are not uniform.
-    // "A can" can be entirely different from one area to another, from a year to another (e.g. shrinkflation), etc.
-    // TODO - have a tooltip to discourage such use on the front end whenever possible
+
 }
