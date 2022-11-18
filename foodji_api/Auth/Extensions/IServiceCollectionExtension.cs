afdc5336bc3e969021e5a93b4827ec4235679477
Sanyella <50803386+Sanyella@users.noreply.github.com>
2022-11-18 14:20:56 -0500

    Firebase validation (#25)
    * Firebase validation
    
    * Comment on Auth scheme
    
    * Relative path for Firebase file
    
    Definitely not the cleanest way to do things, but in line with the limits we put on ourselves for managing secrets and configuration at this time
    
    * Create Auth project in solution
    
    * Use prefered way to access executing directory
    
    See second comment on https://github.com/dotnet/corert/issues/5467
    
    * Fix CI
    
    Co-authored-by: Éloyze Brière-Alix <brie1301@usherbrooke.ca>

---
+++ foodji_api/Auth/Extensions/IServiceCollectionExtension.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Auth.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static void AddFirebaseJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        // This whole manipulation would very likely benefit from better configuration handling,
+        // in addition to secrets handling. This is better left to another task, however.
+        var firebaseCredentialsFileName = configuration["Auth:Firebase:CredentialsFile"];
+        // If the directory of the executing assembly is null, we have bigger problems
+        var firebaseCredentials = Path.Combine(AppContext.BaseDirectory, firebaseCredentialsFileName);
+        
+        // Needs to be created to use the FirebaseAuth.DefaultInstance later, in the handler
+        FirebaseApp.Create(new AppOptions
+        {
+            Credential = GoogleCredential.FromFile(firebaseCredentials)
+        });
+
+        // Possible that the authentication scheme we want to use is not strictly the JwtBearerDefaults one,
+        // though it might also work fine. We can change it if it causes issues down the line. For now, it is simpler
+        // to use that notably to use along with Swagger (and overall seems valid to me, despite uncertainty?).
+        services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddScheme<FirebaseAuthenticationSchemeOptions, FirebaseAuthenticationHandler>(
+                JwtBearerDefaults.AuthenticationScheme, null);
+    }
+}
