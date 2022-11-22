8bc720b2024094840dfc0ab99b5256ba83160e7a
Guillaume Gervais <goglu.gervais@gmail.com>
2022-11-22 13:45:37 -0500

    Change json path for mongo prefix

--- foodji_api/Api/Program.cs
+++ foodji_api/Api/Program.cs
@@ -24,16 +24,16 @@ builder.Services.AddSwaggerGen(c =>
         In = ParameterLocation.Header,
         Description = "JWT Authorization header using the Bearer scheme."
     });
-    
+
     c.AddSecurityRequirement(new OpenApiSecurityRequirement
     {
         {
             new OpenApiSecurityScheme
             {
-                Reference = new OpenApiReference 
-                { 
-                    Type = ReferenceType.SecurityScheme, 
-                    Id = "bearerAuth" 
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "bearerAuth"
                 }
             },
             Array.Empty<string>()
@@ -42,12 +42,16 @@ builder.Services.AddSwaggerGen(c =>
 });
 
 // TODO use secrets - not critical for the moment in local development ONLY
-var connectionAddress = builder.Environment.IsDevelopment() ? "localhost:27017" : Environment.GetEnvironmentVariable("DB_ADDR");
-if(connectionAddress == null)
+var connectionAddress = builder.Environment.IsDevelopment()
+    ? "localhost:27017"
+    : Environment.GetEnvironmentVariable("DB_ADDR");
+
+if (connectionAddress == null)
     throw new Exception("Database address is null");
 
 // Map dependency injection
-builder.Services.SetupInfra($"{builder.Configuration.GetSection("Database:MongoDB")["ConnectionString"]}@{connectionAddress}");
+builder.Services.SetupInfra(
+    $"{builder.Configuration.GetSection("MongoDB")["ConnectionString"]}@{connectionAddress}");
 
 builder.Services.AddMediatR(Assembly.Load("Application"));
 builder.Services.AddAutoMapper(Assembly.Load("Application"));
@@ -64,6 +68,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+/**
+ * À conserver pour des hostings futurs. Dans certains infras,
+ * on préfèrerait gérer le tout avec le reversed proxy et un certificate manager.
+**/
+//app.UseHttpsRedirection();
 
 app.UseAuthentication();
 
