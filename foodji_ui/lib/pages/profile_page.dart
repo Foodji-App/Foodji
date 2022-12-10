6671a8900f68acfd46fdf7444e92f674e3b44a7d
Mikaël Hubert-Deschamps <mikael.hubert-deschamps@usherbrooke.ca>
2022-12-09 19:23:32 -0500

    Added localization to auth_page.dart (#43)
    Removed unused l10n elements regarding login and registration
    Removed unused Registration page
    Refactored profile page so it is in profile_page.dart instead of navigation_bar.dart

--- foodji_ui/lib/pages/profile_page.dart
+++ foodji_ui/lib/pages/profile_page.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutterfire_ui/i10n.dart';
 
 import '../cubit/app_globals.dart' as globals;
 import '../cubit/app_cubit_states.dart';
@@ -17,15 +20,22 @@ class ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     globals.setActivePage(3);
     return BlocBuilder<AppCubits, CubitStates>(builder: (context, state) {
-      return Scaffold(
-          body: Container(
-              width: double.maxFinite,
-              height: double.maxFinite,
-              decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('img/background-gradient.png'),
-                      fit: BoxFit.fill)),
-              alignment: Alignment.center));
+      return MaterialApp(
+          localizationsDelegates: [
+            AppLocalizations.delegate,
+            FlutterFireUILocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+          ],
+          supportedLocales: AppLocalizations.supportedLocales,
+          theme: ThemeData(fontFamily: 'bauhaus', brightness: Brightness.dark),
+          debugShowCheckedModeBanner: false,
+          home: ProfileScreen(actions: [
+            SignedOutAction((context) {
+              BlocProvider.of<AppCubits>(context).getInitialData();
+            })
+          ]));
     });
   }
 }
